b55b10df70a247ddb5b3e7f219278564d614a445
Cizz22 <cisatraa@gmail.com>
2022-05-09 13:59:45 +0700

    add prpcfile

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname+'/public'));
-app.listen(process.env.port || 3000);
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
   });
 
 
