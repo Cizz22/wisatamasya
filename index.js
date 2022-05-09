c79dd6d3fae29654017cb8e8abac65daccb00414
Cizz22 <cisatraa@gmail.com>
2022-05-09 13:53:48 +0700

    add

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-app.use('/', router);
 app.use(express.static(__dirname+'/public'));
 app.listen(process.env.port || 3000);
 
-console.log('Running at Port 3000');
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+  });
+
+
