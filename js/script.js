e4e6375f2da63c33a8787f89fea2fd1641245d72
Cizz22 <cisatraa@gmail.com>
2022-05-09 12:48:30 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    // Add smooth scrolling to all links
+    $("#go-main").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    $(window).scroll(function(){
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0}, 800);
+        $('html').css("scrollBehavior", "auto");
+    });
+
+
+
+});
